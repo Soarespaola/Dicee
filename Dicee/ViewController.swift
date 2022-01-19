2c6f07f1b8679003eb4509a4d3cc526717580f32
Paola Soares <paolaalcantara1@yahoo.com.br>
2022-01-19 15:00:59 -0300

    Update Constraints

--- Dicee/ViewController.swift
+++ Dicee/ViewController.swift
@@ -5,27 +5,26 @@
 //  Created by Paola Alcantara Soares on 13/01/22.
 //
 
-
 import UIKit
 
 class ViewController: UIViewController {
 
 //Primeiro dado
-    @IBOutlet weak var diceImage1: UIImageView!
+    @IBOutlet weak var diceImageView1: UIImageView!
 //Segundo dado
-    @IBOutlet weak var diceImage2: UIImageView!
-    
+    @IBOutlet weak var diceImageView2: UIImageView!
+
     var leftDiceNumber = 1
     var rightDiceNumber = 5
     
 
-    @IBAction func rollButton(_ sender: UIButton) {
+    @IBAction func rollButtonPressed(_ sender: UIButton) {
         
         let diceArray = [#imageLiteral(resourceName: "DiceOne"), #imageLiteral(resourceName: "DiceTwo"), #imageLiteral(resourceName: "DiceThree"), #imageLiteral(resourceName: "DiceFour"), #imageLiteral(resourceName: "DiceFive"), #imageLiteral(resourceName: "DiceSix")]
         
                                                 //Para os dados rodarem de forma aleatória
-        diceImage1.image = diceArray[Int.random(in: 0...5)]
-        diceImage2.image = diceArray[Int.random(in: 0...5)]
+        diceImageView1.image = diceArray[Int.random(in: 0...5)]
+        diceImageView2.image = diceArray[Int.random(in: 0...5)]
         
         //uma forma mais sucinta de escrever diceArray.random
     
